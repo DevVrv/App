81fa693dfa5c5a42ffc61ab8c2cc65f761b96d34
Roman <107090578+DevVrv@users.noreply.github.com>
2023-01-19 00:16:09 +0600

    app 19_01

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,17 +1,9 @@
 import React from "react"
-import Header from "./components/Header"
-// import { BasicModal } from "./components/plugins/Modals"
-// import { AlertSmal } from "./components/plugins/Alerts"
-// import { useState } from "react"
-// import { toggler } from "./tools/handlerTools"
 
 function App() {
     return (
         <div className="wrapper">
 
-            {/* Header */}
-            <Header />
-            
         </div>
     )
 }
--- app/src/index.js
+++ app/src/index.js
@@ -4,8 +4,8 @@ import reportWebVitals from './reportWebVitals';
 import App from './App';
 
 // import bootstrap styles
-import './assets/vendors/bootstrap-5.1.3-dist/css/bootstrap-grid.min.css'
-import './assets/vendors/bootstrap-5.1.3-dist/css/bootstrap-utilities.min.css'
+import './assets/css/bootstrap-5.1.3/bootstrap-grid.min.css'
+import './assets/css/bootstrap-5.1.3/bootstrap-utilities.min.css'
 
 // import base styles
 import './assets/css/style.min.css'
